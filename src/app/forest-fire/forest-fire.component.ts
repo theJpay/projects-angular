01d2a8791a0a47d821057b068c58d3ab37f961c4
Jules Paris <jules.paris@wanadoo.fr>
2018-08-01 19:52:43 +0200

    ajout de la boucle de fonction avec to

--- src/app/forest-fire/forest-fire.component.ts
+++ src/app/forest-fire/forest-fire.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { initDomAdapter } from "../../../node_modules/@angular/platform-browser/src/browser";
 
 @Component({
   selector: "app-forest-fire",
@@ -22,6 +21,15 @@ export class ForestFireComponent implements OnInit {
     return Math.random();
   }
 
+  private getRandomRange(min, max): number {
+    if (max > min) {
+      return min + Math.random()*(max-min);
+    }
+    else {
+      return this.getRandomRange(max, min);
+    }
+  }
+
   public getState(i: number) {
     switch (i) {
       case 0:
@@ -38,13 +46,17 @@ export class ForestFireComponent implements OnInit {
   private init() {
     this.map = [];
     let frame;
+    if (this.x <= 0) this.x = 1;
+    if (this.x > 250) this.x = 250;
+    if (this.y <= 0) this.y = 1;
+    if (this.y > 250) this.y = 250;
+    if (this.interval < 100) this.interval = 100;
     for (let i = 0; i < this.y; i++) {
       this.map.push([]);
       for (let j = 0; j < this.x; j++) {
         if (this.getRandom() > 0.9) {
           frame = 1;
-        }
-        else {
+        } else {
           frame = 0;
         }
         this.map[i].push(frame);
@@ -52,17 +64,31 @@ export class ForestFireComponent implements OnInit {
     }
   }
 
-  public startStop() {
-    if (this.button) {
-      this.init();
-      this.started = true;
+  private loopTransition() {
+    for (let row of this.map) {
+      for (let i in row) {
+        row[i] = parseInt(this.getRandomRange(0,4).toString());
+      }
     }
+    setTimeout(() => {
+      if (this.started) {
+        this.loopTransition();
+      }
+    }, this.interval);
+  }
+
+  public start() {
+    this.init();
+    this.started = true;
+    this.loopTransition();
   }
 
   public toggle() {
     this.button = !this.button;
     if (!this.button) {
       this.started = false;
+    } else {
+      this.start();
     }
   }
 }
